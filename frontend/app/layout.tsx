c878bb682c269c87b44ae7338561ba451bb113f4
417266 <165021594+417266@users.noreply.github.com>
2025-06-07 09:44:18 +0200

    Front end: Add dark mode

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,20 @@
 import { Toaster } from "sonner";
 import "./globals.css";
 import { ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
 
 const RootLayout = ({ children }: { children: ReactNode }) => (
-  <html lang="en">
+  <html lang="en" suppressHydrationWarning>
     <body>
-      {children}
-      <Toaster richColors />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster richColors />
+      </ThemeProvider>
     </body>
   </html>
 );
