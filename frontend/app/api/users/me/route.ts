165dc3a43f74b4754261e53fd0bb64188c305e2b
417266 <165021594+417266@users.noreply.github.com>
2025-06-07 14:19:09 +0200

    Front end: Extend `api/users/me` route handler with `PUT` and `DELETE` support

--- frontend/app/api/users/me/route.ts
+++ frontend/app/api/users/me/route.ts
@@ -6,3 +6,27 @@ export const GET = async () =>
       Authorization: `Bearer ${(await cookies()).get("access_token")?.value}`,
     },
   });
+
+export const PUT = async (request: Request) => {
+  const { displayName, email, password } = await request.json();
+  return fetch(`${process.env.API_BASE_URL}/users/me`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${(await cookies()).get("access_token")?.value}`,
+    },
+    body: JSON.stringify({
+      email,
+      display_name: displayName,
+      password,
+    }),
+  });
+};
+
+export const DELETE = async () =>
+  fetch(`${process.env.API_BASE_URL}/users/me`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${(await cookies()).get("access_token")?.value}`,
+    },
+  });
