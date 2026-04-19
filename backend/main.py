0659f484032c2d2b2b2e65480628cfbbc10a2b15
shevaroma <sheva1501@gmail.com>
2026-04-19 14:43:03 +0200

    Back end: Update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://project-1lm4u.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
